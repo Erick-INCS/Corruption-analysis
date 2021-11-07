85b0f971e3b21400b6063d7f0f721e9aa33b6839
Erick Arroyo <incspnsoft@gmail.com>
2021-11-06 21:38:22 -0700

    [Add] Uitls file

---
+++ utils/project_utils.py
@@ -0,0 +1,47 @@
+""""Utilidades para este proyecto."""
+
+def renom_estados(name):
+  estados = {
+    'Michoacán de Ocampo': 'Michoacán', 
+    'México': 'Estado de México', 
+    'Veracruz Ignacio de la Llave': 'Veracruz',
+    'Coahuila de Zaragoza': 'Coahuila'}
+
+  if name in estados:
+    return estados[name]
+  
+  return name
+
+
+
+def get_money(text):
+
+  if pd.isna(text):    
+    return np.nan
+  re_dinero = re.compile(r'monto de ([\d|,|\.]+) pesos')
+  res = re_dinero.search(text)
+  if res:
+    re_num = re.compile(r'[^0-9]')
+    return float(re.sub(re_num, '', res.groups()[0][:-3]))
+  return np.nan
+
+  
+def get_ds_asf():
+  ds_asf = pd.read_csv(
+    'https://raw.githubusercontent.com/Erick-INCS/Corruption-analysis/main/datasets/asf_2017-2019.csv',
+    encoding='latin-1')
+  ds_asf['terminado'] = ds_asf['Estado de Trámite'].isin(['Con seguimiento concluido','Conclusión'])
+  ds_asf['desaparecido'] = ds_asf['Texto Acción'].apply(get_money)
+  ds_asf['Entidad Federativa'] = ds_asf['Entidad Federativa'].map(renom_estados)
+  return ds_asf
+
+def describe_chido(df, columnas = []):
+  if columnas:
+    df = df[columnas]
+  descripcion =df.describe()
+  descripcion.loc['IQR'] = descripcion.loc['75%']-descripcion.loc['25%']
+  df['desaparecido'].sort_values(ascending= True)
+  descripcion.loc['media trunca'] = stats.trim_mean(df, .05)
+  descripcion.loc['mediana'] = df.median()
+  descripcion.loc['rango'] = df.max() - df.min()
+  return descripcion
